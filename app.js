61d57cd0e5a31d63c7e8af3bf108182a3e0be2ea
Marco Cedaro <cedmax@users.noreply.github.com>
2019-10-02 13:43:23 +0100

    transform to sober october

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ const data = {
   minutes: null,
   seconds: null,
   savings: null,
-  dollars: null,
+  pounds: null,
   cents: null,
-  dailyCost: 6.375466667,
-  time: 1569369600000 // new Date(2019, 8, 24, 19)
+  dailyCost: 5,
+  time: new Date(2019, 8, 29, 00), // new Date(2019, 8, 24, 19)
 };
 
-const progresses = document.querySelectorAll(".progress[fraction]");
+const progresses = document.querySelectorAll('.progress[fraction]');
 
 update();
 
@@ -28,15 +28,15 @@ function update() {
   data.minutes = seconds / 60;
   data.seconds = seconds;
   data.savings = data.days * data.dailyCost;
-  data.dollars = Math.floor(data.savings)
+  data.pounds = Math.floor(data.savings)
     .toString()
-    .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    .replace(/\B(?=(\d{3})+(?!\d))/g, ',');
   data.cents = (data.savings - Math.floor(data.savings))
     .toFixed(2)
-    .split(".")[1];
+    .split('.')[1];
   updatePies();
-  document.getElementById("dollars").innerText = data.dollars;
-  document.getElementById("cents").innerText = data.cents;
+  document.getElementById('pounds').innerText = data.pounds;
+  document.getElementById('cents').innerText = data.cents;
 
   requestAnimationFrame(update);
 }
@@ -45,16 +45,16 @@ function updatePies() {
   const radius = 25;
   const circumference = radius * 2 * Math.PI;
   progresses.forEach(progress => {
-    const value = data[progress.getAttribute("fraction")];
+    const value = data[progress.getAttribute('fraction')];
     const complete = Math.floor(value);
-    let v = complete.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    let v = complete.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
     if (complete < 10) v = value.toFixed(2);
     if (complete < 1) v = value.toFixed(3);
-    progress.querySelector("h2").innerText = v;
+    progress.querySelector('h2').innerText = v;
     const percent = Math.round((value - complete) * 100 * 10) / 10;
     const offset = circumference - (percent / 100) * circumference;
     progress.querySelector(
-      ".left"
+      '.left'
     ).innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="100" width="100" viewBox="0 0 100 100">
     <circle class="bg" r="${radius}" cx="50" cy="50" />
     <circle
